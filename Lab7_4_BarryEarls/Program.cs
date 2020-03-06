f4157e068c5bd9e919714cb5bf7290931ab52b8e
bearls <61299567+bearls@users.noreply.github.com>
2020-03-05 19:38:10 -0600

    Add files via upload

---
+++ Lab7_4_BarryEarls/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+namespace Lab7_4_BarryEarls
+{ 
+
+ 
+    class Program
+    { 
+    
+        static void Main(string[] args)
+        {
+            DatingProfile andy = new DatingProfile("Andy", "Stitzer", 40, "Male");
+            andy.WriteBio("Love collecting action figures.");
+
+            DatingProfile trish = new DatingProfile("Trish", "Piedmont", 40, "Female");
+            trish.WriteBio("Hot Milf");
+
+            andy.SendMessage("Hello Trish", "Im a virgin.", trish);
+            trish.ReadMessage();
+        }
+    }
+}
---
+++ Lab7_4_BarryEarls/messages.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab7_4_BarryEarls
+{
+
+    class messages
+    {
+        public DatingProfile sender;
+        public string messageTitle;
+        public string messageData;
+        private bool isRead;
+
+        public Messages(DatingProfile sender, string messageTitle, string messageData)
+        {
+            this.sender = sender;
+            this.messageTitle = messageTitle;
+            this.messageData = messageData;
+            isRead = false;
+        }
+
+    }
+
+}
